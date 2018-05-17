446f506da15dbfe4ed5bf667e3df05546d027d03
jerza1 <jrz.bautista@gmail.com>
2018-05-16 20:21:25 -0500

    Pruebas unitarias a las clases ListaCompras y Compras

---
+++ src/main/java/elabuelonicolas/service/compras/ComprasService.java
@@ -0,0 +1,16 @@
+package elabuelonicolas.service.compras;
+ 
+import java.util.List;
+import elabuelonicolas.bd.domain.Compras;
+
+public interface ComprasService {
+	List<Compras> findAll();
+ 
+	void create(Compras compra);
+
+	Compras read(int id);
+
+	void update(Compras compra);
+
+	void delete(int id);
+}

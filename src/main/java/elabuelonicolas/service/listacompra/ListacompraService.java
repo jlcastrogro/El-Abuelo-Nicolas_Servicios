8693300ba06ce71fdf8bb60a0f9ac6088b7e01f2
jlcastrogro <jlcastrogro@gmail.com>
2018-06-01 18:08:51 -0500

    Service listacompra

--- src/main/java/elabuelonicolas/service/listacompra/ListacompraService.java
+++ src/main/java/elabuelonicolas/service/listacompra/ListacompraService.java
@@ -1,17 +1,37 @@
 package elabuelonicolas.service.listacompra;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import elabuelonicolas.bd.domain.Listacompra;
  
 public interface ListacompraService {
 	List<Listacompra> findAll();
 
-	void create(Listacompra lc);
+	void create(Listacompra listacompra);
 
-	Listacompra read(int id); 
+	Listacompra read(int id);
 
-	void update(Listacompra lc);
+	List<Listacompra> readByIdCompra(int idcompra);
+
+	List<Listacompra> readByIdProducto(int idproducto);
+
+	List<Listacompra> readByCantidad(int cantidad);
+
+	List<Listacompra> readBySubtotal(double subtotal);
+
+	void update(Listacompra listacompra);
+
+	void updateIdCompra(@Param("id") int id, @Param("idcompra") int idcompra);
+
+	void updateIdProducto(@Param("id") int id, @Param("idproducto") int idproducto);
+
+	void updateCantidad(@Param("id") int id, @Param("cantidad") int cantidad);
+
+	void updateSubtotal(@Param("id") int id, @Param("subtotal") double subtotal);
 
 	void delete(int id);
 
+	Listacompra last();
 }
--- src/main/java/elabuelonicolas/service/listacompra/ListacompraServiceImpl.java
+++ src/main/java/elabuelonicolas/service/listacompra/ListacompraServiceImpl.java
@@ -24,12 +24,57 @@ public class ListacompraServiceImpl implements ListacompraService{
 		return listacompraDao.read(id);
 	}
 
+	@Override
+	public List<Listacompra> readByIdCompra(int idcompra) {
+		return listacompraDao.readByIdCompra(idcompra);
+	}
+
+	@Override
+	public List<Listacompra> readByIdProducto(int idproducto) {
+		return listacompraDao.readByIdProducto(idproducto);
+	}
+
+	@Override
+	public List<Listacompra> readByCantidad(int cantidad) {
+		return listacompraDao.readByCantidad(cantidad);
+	}
+
+	@Override
+	public List<Listacompra> readBySubtotal(double subtotal) {
+		return listacompraDao.readBySubtotal(subtotal);
+	}
+	
 	public void update(Listacompra compra) {
 		listacompraDao.update(compra);
 	}
 
+	@Override
+	public void updateIdCompra(int id, int idcompra) {
+		listacompraDao.updateIdProducto(id, idcompra);
+	}
+
+	@Override
+	public void updateIdProducto(int id, int idproducto) {
+		listacompraDao.updateIdProducto(id, idproducto);
+	}
+
+	@Override
+	public void updateCantidad(int id, int cantidad) {
+		listacompraDao.updateCantidad(id, cantidad);
+	}
+
+	@Override
+	public void updateSubtotal(int id, double subtotal) {
+		listacompraDao.updateSubtotal(id, subtotal);
+	}
+	
 	public void delete(int id) {
 		listacompraDao.delete(id);
+	}
+
+	@Override
+	public Listacompra last() {
+		return listacompraDao.last();
 	}
 	
 }

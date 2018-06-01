7928e575b7b0d506143e97badb62f23fe10e663c
jlcastrogro <jlcastrogro@gmail.com>
2018-06-01 18:15:54 -0500

    Service listaventa

--- src/main/java/elabuelonicolas/service/producto/ProductoService.java
+++ src/main/java/elabuelonicolas/service/producto/ProductoService.java
@@ -1,6 +1,9 @@
 package elabuelonicolas.service.producto;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import elabuelonicolas.bd.domain.Producto;
 
 public interface ProductoService {
@@ -9,8 +12,26 @@ public interface ProductoService {
 	void create(Producto producto);
 
 	Producto read(int id);
+	
+	List<Producto> readByTipo(String tipo);
+	
+	List<Producto> readByMarca(String marca);
+	
+	List<Producto> readByCosto(Double costo);
+	
+	List<Producto> readByExistencia(int existencia);
 
 	void update(Producto producto);
 
+	void updateTipo(@Param("id") int id, @Param("tipo") String tipo);
+	
+	void updateMarca(@Param("id") int id, @Param("marca") String marca);
+	
+	void updateCosto(@Param("id") int id, @Param("costo") Double costo);
+	
+	void updateExistencia(@Param("id") int id, @Param("existencia") int existencia);
+	
 	void delete(int id);
+	
+	Producto last();
 }
--- src/main/java/elabuelonicolas/service/producto/ProductoServiceImpl.java
+++ src/main/java/elabuelonicolas/service/producto/ProductoServiceImpl.java
@@ -24,11 +24,56 @@ public class ProductoServiceImpl implements ProductoService {
 		return productoDao.read(id);
 	}
 
+	@Override
+	public List<Producto> readByTipo(String tipo) {
+		return productoDao.readByTipo(tipo);
+	}
+
+	@Override
+	public List<Producto> readByMarca(String marca) {
+		return productoDao.readByMarca(marca);
+	}
+
+	@Override
+	public List<Producto> readByCosto(Double costo) {
+		return productoDao.readByCosto(costo);
+	}
+	
+	@Override
+	public List<Producto> readByExistencia(int existencia) {
+		return productoDao.readByExistencia(existencia);
+	}
+	
 	public void update(Producto producto) {
 		productoDao.update(producto);
 	}
 
+	@Override
+	public void updateTipo(int id, String tipo) {
+		productoDao.updateTipo(id, tipo);
+	}
+
+	@Override
+	public void updateMarca(int id, String marca) {
+		productoDao.updateMarca(id, marca);
+	}
+
+	@Override
+	public void updateCosto(int id, Double costo) {
+		productoDao.updateCosto(id, costo);
+	}
+
+	@Override
+	public void updateExistencia(int id, int existencia) {
+		productoDao.updateExistencia(id, existencia);
+	}
+	
 	public void delete(int id) {
 		productoDao.delete(id);
+	}
+
+	@Override
+	public Producto last() {
+		return productoDao.last();
 	}
 }

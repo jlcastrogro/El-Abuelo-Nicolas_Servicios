c8646e76436dbf4824666c2c4a54024c3eaf845a
jlcastrogro <jlcastrogro@gmail.com>
2018-06-06 19:05:40 -0500

    Cambio de clase de fecha

--- src/main/java/elabuelonicolas/service/compra/CompraService.java
+++ src/main/java/elabuelonicolas/service/compra/CompraService.java
@@ -1,6 +1,6 @@
 package elabuelonicolas.service.compra;
  
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
--- src/main/java/elabuelonicolas/service/compra/CompraServiceImpl.java
+++ src/main/java/elabuelonicolas/service/compra/CompraServiceImpl.java
@@ -1,6 +1,6 @@
 package elabuelonicolas.service.compra;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
